48c55488004274c2bef79df63ebaf7908c83a5d4
mohamed <mmm.medox@gmail.com>
2025-05-20 05:51:05 +0300

    add readme file

---
+++ app/src/main/java/com/dev3mk/awraqi/services/MessagingService.kt
@@ -0,0 +1,65 @@
+package com.dev3mk.awraqi.services
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.net.toUri
+import com.dev3mk.awraqi.R
+import com.dev3mk.awraqi.util.Constants
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val title = message.notification?.title.toString()
+        val body = message.notification?.body.toString()
+        val link = message.data["link"].toString()
+        println("link: $link")
+        Log.d(
+            "ggg",
+            "notification_title: $title    notification_message: $body   link: $link"
+        )
+        if (title != "null" && body != "null")
+            sendNotification(title, body, link)
+
+    }
+
+    private fun sendNotification(title: String, body: String, link: String) {
+
+        val intent = Intent(Intent.ACTION_VIEW, link.toUri())
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val nm: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                Constants.NOTIFICATION_CHANNEL_ID,
+                Constants.NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.setShowBadge(true)
+            channel.enableLights(true)
+            nm.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.drawable.baseline_auto_stories)
+            .setContentIntent(pi)
+            .setAutoCancel(true)
+            .build()
+
+        nm.notify(Random.nextInt(), builder)
+    }
+}
